91c3f6fbcb9e00404a689f856b24746603873a12
Saurabh Bomble <saurabhbomble107@gmail.com>
2022-08-22 20:20:33 +0530

    chore: basic fixes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+import { __prod__ } from './utils/constant';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env.development',
+      ignoreEnvFile: __prod__,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      logging: !__prod__,
+      synchronize: !__prod__,
+      entities: [],
+    }),
+    UserModule,
+  ],
 })
 export class AppModule {}
